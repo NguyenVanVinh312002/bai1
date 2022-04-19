bf48d46634575bcd8caf50b6fb041659e67adf03
NguyenVanVinh312002 <97501964+NguyenVanVinh312002@users.noreply.github.com>
2022-04-19 14:36:51 +0700

    Add files via upload

---
+++ BaiThucHanh1904/src/test.java
@@ -0,0 +1,46 @@
+
+public class test {
+
+	public static void main(String[] args) {
+//	 Person p1= new Person(null, 0);
+//	 p1.nhapTen();
+//	 p1.nhapTuoi();
+//	 p1.inTT();
+		
+// 	 Student s1=new Student(null, 0, null, 0)	;
+//	 s1.nhapLop();
+//	 s1.nhapDtb();
+//	 s1.nhapTen();
+//	 s1.nhapTuoi();
+//	 s1.inTT();
+		
+//	 OfflineStudent off=new OfflineStudent(null, 0, null, 0, null);
+//   off.nhapTen();
+//	 off.nhapTuoi();
+//	 off.nhapLop();
+//	 off.nhapDtb();
+//	 off.nhapPhuongTienDiChuyen();
+//	 off.inTT();
+		
+//	 OnlineStudent onl=new OnlineStudent(null, 0, null, 0, null);
+//	 onl.nhapTen();
+//	 onl.getLop();
+//	 onl.nhapTuoi();
+//	 onl.nhapDtb();
+//	 onl.nhapPhuongTienhocOnl();
+//	 onl.inTT();
+	
+//	 PartTimeEmployee pe= new PartTimeEmployee(null, 0, null, 0, 0, 0)	;
+//	 pe.nhapTen();
+//	 pe.nhapTuoi();
+//	 pe.nhapTenCuaHangLamViec();
+//	 pe.nhapSoNgayLam();
+//	 pe.nhapGioLam();
+//	 pe.tinhluong();
+//	 pe.inTT();
+		
+	 
+		
+	}
+
+}
