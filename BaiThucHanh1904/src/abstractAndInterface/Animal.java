526885a7c86a2ff6b73361cd0d83c5fe3ccb491f
NguyenVanVinh312002 <97501964+NguyenVanVinh312002@users.noreply.github.com>
2022-04-19 14:41:23 +0700

    Delete BaiThucHanh1904 directory

--- BaiThucHanh1904/src/abstractAndInterface/Animal.java
+++
@@ -1,9 +0,0 @@
-package abstractAndInterface;
-
-public abstract class Animal {
-	abstract void Eat();
-	abstract void Sleep();
-	abstract void Walk();
-	abstract void Run();
-	abstract void Roar();
-}
